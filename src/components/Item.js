1352b8d0bdccc7405f7952013935b041c559fe88
Peyton Meredith <peymeredith@gmail.com>
2023-10-04 19:04:33 -0500

    react state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [addItem, setAddItem] = useState("add")
+  
+  const handleAddItem = () => {
+    setAddItem(!addItem)
+  }
+
+  const buttonText = addItem ? "Add to Cart" : "Remove From Cart"
+  const liClassName = addItem ? "" : "in-cart"
+  const buttonClassName = addItem ? "add" : "remove"
+  
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={handleAddItem} >{buttonText}</button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  console.log("og selected category", selectedCategory)
+  
+  const handleChange = (e) => {
+  
+    setSelectedCategory(e.target.value)
+    // const renderCategoryItems = filteredArray.map((filteredItem))
+  }
+  const filteredArray = items.filter(item => {
+    if (selectedCategory === "All"){
+      return true
+    } else {
+      return item.category.includes(selectedCategory)
+    }
+  } )
+  // console.log(filteredArray)
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredArray.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
